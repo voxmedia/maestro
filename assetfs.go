8d5de2812108d3e2459a075ed9dc665060cd7422
Grisha Trubetskoy <grisha.trubetskoy@voxmedia.com>
2019-06-07 16:21:30 -0400

    Build condition should be at the top

--- assetfs.go
+++ assetfs.go
@@ -1,3 +1,5 @@
+// +build !builtinassets
+
 /* Copyright 2019 Vox Media, Inc.
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,8 +16,6 @@
 
 // Fake assetfs symbol satisfaction. Does not build when the
 // builtin-assets tag is specified.
-
-// +build !builtinassets
 
 package main
 
